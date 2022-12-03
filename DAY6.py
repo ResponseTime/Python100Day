215c793df53a98718c298677bc7b8153c9295b6b
Looper <aayush.bhattacharjee2002@gmail.com>
2022-12-03 22:17:30 +0530

    DAY6

---
+++ DAY6.py
@@ -0,0 +1,22 @@
+a = 7
+b = "name"
+c = None
+d = 43.666
+e = False
+f = complex(2, 34)
+list2 = [2, 3, 5, [34, 65, 2], "name is this"]
+tup2 = (34, 65, 76, 32, 44, [54, 7, 3], "name")
+dict2 = {
+    "name": "aayush",
+    "age": 20,
+    "ty": "wlr"
+}
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+print(e, type(e))
+print(f, type(f))
+print(list2, type(list2))
+print(tup2, type(tup2))
+print(dict2, type(dict2))
