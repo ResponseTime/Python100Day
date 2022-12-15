d337c96c216308df99fad03e28062d2d70498114
Looper <aayush.bhattacharjee2002@gmail.com>
2022-12-15 18:49:18 +0530

    DAY18

---
+++ DAY18.py
@@ -0,0 +1,24 @@
+i = 0
+while i <= 20:
+    print(i)
+    i += 1
+
+k = 6
+
+while k > 0:
+    print(k)
+    k -= 1
+else:
+    print("The loop has ended because the condition is no longer true")
+
+# j = 0
+# print(j)
+# j += 1
+# while j >= 10:
+#     print(j)
+j = 20
+while True:
+    print(j)
+    j += 1
+    if j >= 10:
+        break
