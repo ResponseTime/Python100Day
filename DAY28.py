b37eb87205d11ecb93e70f05b40d69783e466f5f
Looper <aayush.bhattacharjee2002@gmail.com>
2022-12-25 19:40:41 +0530

    DAY28

---
+++ DAY28.py
@@ -0,0 +1,5 @@
+sen = "MY name is {0} and I am {1} years old"
+print(sen.format("John", 25))
+print(f"The price is {1000.666:.2f}")
+ser = "MY name is {{0}} and I am {{1}} years old"
+print(ser.format("John", 25))
