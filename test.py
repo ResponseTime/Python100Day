f5eeff37c391497f8789ff2d205edaf5a8faedc4
Looper <aayush.bhattacharjee2002@gmail.com>
2022-12-26 23:16:10 +0530

    Some test runs

--- test.py
+++ test.py
@@ -1,46 +1,84 @@
-import time as t
-import getpass
+# import time as t
+# import getpass
 
 
-class User():
-    Username = None
-    Password = None
+# class User():
+#     Username = None
+#     Password = None
 
-    def __init__(self):
-        file = open('details.txt', 'a')
-        file.close()
+#     def __init__(self):
+#         file = open('details.txt', 'a')
+#         file.close()
 
-    def signup(self):
-        val = input("Enter username ")
-        val2 = getpass.getpass()
-        self.Username = val
-        self.Password = val2
-        with open('details.txt', 'a') as detail:
-            detail.writelines(self.Username+":"+self.Password)
-            detail.write('\n')
+#     def signup(self):
+#         val = input("Enter username ")
+#         val2 = getpass.getpass()
+#         self.Username = val
+#         self.Password = val2
+#         with open('details.txt', 'a') as detail:
+#             detail.writelines(self.Username+":"+self.Password)
+#             detail.write('\n')
 
-    def login(self):
-        while True:
-            val = input("Enter username ")
-            val2 = getpass.getpass()
-            with open('details.txt', 'r') as details:
-                data_obj = []
-                for i in details.readlines():
-                    data_obj.append(i.rstrip('\n'))
-                if f"{val}:{val2}" in data_obj:
-                    break
-                else:
-                    print("Invalid username or password ")
-                    value = input("To sign up enter 0 ")
-                    if value == '0':
-                        self.signup()
+#     def login(self):
+#         while True:
+#             val = input("Enter username ")
+#             val2 = getpass.getpass()
+#             with open('details.txt', 'r') as details:
+#                 data_obj = []
+#                 for i in details.readlines():
+#                     data_obj.append(i.rstrip('\n'))
+#                 if f"{val}:{val2}" in data_obj:
+#                     break
+#                 else:
+#                     print("Invalid username or password ")
+#                     value = input("To sign up enter 0 ")
+#                     if value == '0':
+#                         self.signup()
 
 
-print("Enter exit() to exit the terminal")
-User = User()
-User.login()
-while True:
-    val = input("$ ")
-    if val == 'exit()':
-        break
-    t.sleep(0.2)
+# print("Enter exit() to exit the terminal")
+# User = User()
+# User.login()
+# while True:
+#     val = input("$ ")
+#     if val == 'exit()':
+#         break
+#     t.sleep(0.2)
+import tkinter as tk
+
+class App:
+    def __init__(self, root):
+        # Create a label to display the input
+        self.label = tk.Label(root, text="")
+        self.label.pack()
+
+        # Create a text entry box for the user to input
+        self.entry = tk.Entry(root)
+        self.entry.pack()
+
+        # Create a button to submit the input
+        self.button = tk.Button(root, text="Submit", command=self.on_submit)
+        self.button.pack()
+
+    def on_submit(self):
+        # Get the input from the text entry box
+        input_text = self.entry.get()
+
+        # Set the label to display the input
+        self.label.config(text=input_text)
+
+# Create the root window
+root = tk.Tk()
+
+# Create an instance of the App class
+app = App(root)
+
+# Run the main loop
+root.mainloop()
+
+
+
+
+
+
+
